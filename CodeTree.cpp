01c5ee381c72c88433b684e303f7fa68ce8b3874
luigi-santos <lusan359@gmail.com>
2024-12-01 19:58:32 -0800

    Update CodeTree.cpp

--- CodeTree.cpp
+++ CodeTree.cpp
@@ -4,3 +4,12 @@
 #include "StringBinaryTree.h"
 
 using namespace std;
+
+void displayMenu() {
+  cout << "1. Add a code" << endl;
+  cout << "2. Delete a code" << endl;
+  cout << "3. Search for a code" << endl;
+  cout << "4. Modify a code" << endl;
+  cout << "5. Display the codes in order" << endl;
+  cout << "6. Quit" << endl;
+}
